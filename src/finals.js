c1edd139c88c9888d481eb347d01b4606fb27543
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-03 18:45:25 +0300

    add finals.js

---
+++ src/finals.js
@@ -0,0 +1,6 @@
+const STATUS_NEW = "new";
+const STATUS_INPROCESS = "in process";
+const STATUS_REJECT = "reject";
+const STATUS_CLOSED = "closed";
+
+module.exports = { STATUS_NEW, STATUS_INPROCESS, STATUS_REJECT, STATUS_CLOSED };
