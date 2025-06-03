fad82a33fcf318dda4f6bcf59cc28963c6a93d04
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-03 15:01:11 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var path = require('path');
+var createRoute = require('./routes/create');
 
-var indexRouter = require('./routes/index');
 
 var app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', createRoute);
 
 module.exports = app;
