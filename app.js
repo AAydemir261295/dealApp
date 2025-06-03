c3abd8d9a63c6b6f84d80363e9fa9adb60b08d14
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-03 17:18:50 +0300

    add deal reject route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var createRoute = require('./routes/create');
 var pickupRoute = require("./routes/pickUp");
 var closeRoute = require("./routes/close");
+var rejectRoute = require("./routes/reject");
 
 var app = express();
 
@@ -13,6 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/create', createRoute);
 app.use('/pickup', pickupRoute);
 app.use('/close', closeRoute);
+app.use('/reject', rejectRoute);
 
 
 
