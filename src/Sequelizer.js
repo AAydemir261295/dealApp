b1e5f10c2e79dcde56130c68284f271e809b6114
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-04 14:31:34 +0300

    rename sequelizer.js -> Sequelizer.js

---
+++ src/Sequelizer.js
@@ -0,0 +1,36 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
+const sequelize = new Sequelize("deals", "qkpostgres", "567607", {
+    host: "localhost",
+    dialect: "postgres"
+})
+
+// class Deals extends Model { }
+
+const Deals = sequelize.define("deals",
+    {
+        deal_id: {
+            type: DataTypes.INTEGER,
+            autoIncrementIdentity: true
+        },
+        deal_timestamp: {
+            type: DataTypes.BIGINT,
+        },
+        deal_theme: {
+            type: DataTypes.STRING()
+        },
+        deal_text: { type: DataTypes.STRING(512) },
+        deal_status: { type: DataTypes.STRING(64) },
+        deal_solution_text: { type: DataTypes.STRING(512) }
+    },
+    {
+        createdAt: false,
+        updatedAt: false,
+    }
+)
+
+Deals.removeAttribute('id')
+
+
+// module.exports = sequelize;
+module.exports = { sequelize, Deals };
